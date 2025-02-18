5fa3dd6a67c18916ec9df2d78db0f72291561433
Leonardo Novaes <leonardoldenovaes@gmail.com>
2025-02-18 09:17:33 -0300

    tri ci 4

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  projectId: "f7fy29",
   e2e: {
     baseUrl: "https://www.saucedemo.com/", // Altere para a URL da sua aplicação
     supportFile: "cypress/support/e2e.ts",
-    specPattern: "cypress/e2e/**/*.cy.ts"
-  }
+    specPattern: "cypress/e2e/**/*.cy.ts",
+  },
 });
